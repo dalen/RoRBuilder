f1865901b5300fbf14890a0e5d9e0f5eb2480e44
Ramasee <Ramasee@yahoo.com>
2018-10-10 20:52:05 -0400

    Update News.js

--- src/js/components/News.js
+++ src/js/components/News.js
@@ -8,6 +8,11 @@ const News = () =>
       <div className={css.item}>
       <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
       <div className={css.itemDate}>10/08/2018</div>
+      <span className={css.itemText}>Updated SW. Changed the home page to reference new nomenclature for buffs/debuffs.</span>
+    </div>
+      <div className={css.item}>
+      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
+      <div className={css.itemDate}>10/08/2018</div>
       <span className={css.itemText}>Updated WH, and Engi. A few attempts at addressing moblie user issues.</span>
     </div>
       <div className={css.item}>
@@ -28,32 +33,7 @@ const News = () =>
     <div className={css.item}>
       <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
       <div className={css.itemDate}>9/26/2018</div>
-      <span className={css.itemText}>Testing ability to update the career pages.</span>
-    </div>
-    <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>4/11/2017</div>
-      <span className={css.itemText}>Updates careers inline with <a href="https://www.returnofreckoning.com/forum/viewtopic.php?f=42&t=23732" target="blank">latest patch</a>.</span>
-    </div>
-    <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>4/11/2017</div>
-      <span className={css.itemText}>A couple of UI improvements: Sidebar menu is now ordered in a sane manner, Mastery tree and abilities are now clickable.</span>
-    </div>
-    <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>30/10/2017</div>
-      <span className={css.itemText}>Major update of skills and abilities of all careers.</span>
-    </div>
-    <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>12/6/2016</div>
-      <span className={css.itemText}>A number of UI improvements to Mastery area.</span>
-    </div>
-    <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>10/6/2016</div>
-      <span className={css.itemText}>First major update. Mastery abilities can now be activated independent of selection. Interface reskinned.</span>
+      <span className={css.itemText}>Created new app off of old one.</span>
     </div>
     <div className={css.item}>
       <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
