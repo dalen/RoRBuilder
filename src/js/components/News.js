0c1522a4448fe76ab82d06e2800154177b0c2681
Ramasee <Ramasee@yahoo.com>
2018-10-19 05:49:48 -0400

    Update News.js

--- src/js/components/News.js
+++ src/js/components/News.js
@@ -7,6 +7,11 @@ const News = () =>
     <div className={css.heading}>Latest updates</div>
       <div className={css.item}>
       <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
+      <div className={css.itemDate}>10/19/2018</div>
+      <span className={css.itemText}>Fixed SH, Updated BO, Updated Magus, Updated WL to proposed changes. Ate a chaos bagel, it was delicious.</span>
+    </div>
+      <div className={css.item}>
+      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
       <div className={css.itemDate}>10/08/2018</div>
       <span className={css.itemText}>Updated SW. Changed the home page to reference new nomenclature for buffs/debuffs.</span>
     </div>
