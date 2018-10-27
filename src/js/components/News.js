50377c970aba5e4801e9fb7c6c4ca1c8d4544def
Ramasee <Ramasee@yahoo.com>
2018-10-27 18:58:40 -0400

    Update News.js

--- src/js/components/News.js
+++ src/js/components/News.js
@@ -7,33 +7,8 @@ const News = () =>
     <div className={css.heading}>Latest updates</div>
       <div className={css.item}>
       <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>10/19/2018</div>
-      <span className={css.itemText}>Fixed SH, Updated BO, Updated Magus, Updated WL to proposed changes. Ate a chaos bagel, it was delicious.</span>
-    </div>
-      <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>10/08/2018</div>
-      <span className={css.itemText}>Updated SW. Changed the home page to reference new nomenclature for buffs/debuffs.</span>
-    </div>
-      <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>10/08/2018</div>
-      <span className={css.itemText}>Updated WH, and Engi. A few attempts at addressing moblie user issues.</span>
-    </div>
-      <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>10/04/2018</div>
-      <span className={css.itemText}>Updated KotBS, SH, SM, and WP</span>
-    </div>
-      <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>10/01/2018</div>
-      <span className={css.itemText}>All careers now start at r40/rr40+.</span>
-    </div>
-    <div className={css.item}>
-      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>9/27/2018</div>
-      <span className={css.itemText}>Updated AM, BG, BW, DoK, IB, RP, SL, and WE careers.</span>
+      <div className={css.itemDate}>10/27/2018</div>
+      <span className={css.itemText}>All classes but Chosen and WL updated. Code handed off to RoR server devs</span>
     </div>
     <div className={css.item}>
       <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
