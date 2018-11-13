f460e2a14432ad9e7a31e077df3ddf52a5a879c0
Unknown <natherul0@gmail.com>
2018-11-13 10:32:01 +0100

    updated news to follow the correct format of day/month/year

--- src/js/components/News.js
+++ src/js/components/News.js
@@ -7,22 +7,22 @@ const News = () =>
     <div className={css.heading}>Latest updates</div>
 	<div className={css.item}>
       <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>10/27/2018</div>
+      <div className={css.itemDate}>13/11/2018</div>
       <span className={css.itemText}>Updated all but WL. If anyone want to help with this please contact Natherul.</span>
     </div>
       <div className={css.item}>
       <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>10/27/2018</div>
+      <div className={css.itemDate}>27/10/2018</div>
       <span className={css.itemText}>All classes but Chosen and WL updated. Code handed off to RoR server devs</span>
     </div>
     <div className={css.item}>
       <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>9/26/2018</div>
+      <div className={css.itemDate}>26/9/2018</div>
       <span className={css.itemText}>Created new app off of old one.</span>
     </div>
     <div className={css.item}>
       <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
-      <div className={css.itemDate}>1/6/2016</div>
+      <div className={css.itemDate}>6/1/2016</div>
       <span className={css.itemText}>We are live :)</span>
     </div>
   </div>;
