c11bc8cfa0b351e31937c0309187c4d9a95c0783
Erik Dalén <erik.gustav.dalen@gmail.com>
2019-01-24 21:08:01 +0100

    Remove postcss-nested-valus and postcss-calc

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,12 @@
 module.exports = {
   plugins: [
     require('postcss-modules-values'),
-    require('postcss-nested'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009',
       },
       stage: 3,
     }),
-    require('postcss-calc')({ mediaQueries: true }),
     require('cssnano')({ preset: 'default' }),
   ],
 };
