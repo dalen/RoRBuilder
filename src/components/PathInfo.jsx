3eb1ae1b6670a2249906e74571507f5231dc001e
Erik Dalén <erik.gustav.dalen@gmail.com>
2019-01-24 22:08:39 +0100

    Pretty close to working

---
+++ src/components/PathInfo.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import classNames from 'classnames';
+import Popover from './Popover';
+import css from '../css/components/PathInfo.css';
+
+class PathInfo extends React.Component {
+  /*
+  infoHovered = info is currently in hover state
+  */
+  constructor(props) {
+    super(props);
+    this.hoverOver = this.hoverOver.bind(this);
+    this.hoverOut = this.hoverOut.bind(this);
+
+    this.state = {
+      hovered: false,
+    };
+  }
+
+  hoverOver() {
+    this.setState({
+      hovered: true,
+    });
+  }
+
+  hoverOut() {
+    this.setState({
+      hovered: false,
+    });
+  }
+
+  renderPopoverPrimary() {
+    if (this.props.pathPopover.primary) {
+      return (
+        <div className={css.popoverPrimary}>
+          {this.props.pathPopover.primary}
+        </div>
+      );
+    }
+    return false;
+  }
+
+  renderPopoverSecondary() {
+    if (this.props.pathPopover.secondary) {
+      return (
+        <div className={css.popoverSecondary}>
+          {this.props.pathPopover.secondary}
+        </div>
+      );
+    }
+    return false;
+  }
+
+  renderPopoverContent() {
+    return (
+      <div>
+        {this.renderPopoverPrimary()}
+        {this.renderPopoverSecondary()}
+      </div>
+    );
+  }
+
+  render() {
+    const infoClass = classNames({
+      'is-hovered': this.state.hovered,
+      popover__parent: true,
+    });
+    return (
+      <div className={infoClass} ref="popoverParent">
+        <div
+          className={css.container}
+          onMouseOver={this.hoverOver}
+          onMouseOut={this.hoverOut}
+        >
+          <div className={css.icon}>?</div>
+        </div>
+        <Popover
+          content={this.renderPopoverContent()}
+          alignment="top"
+          activate={this.state.hovered}
+        />
+      </div>
+    );
+  }
+}
+
+export default PathInfo;
---
+++ src/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import promise from 'redux-promise';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import './css/entry.css';
+
+// For Google Analytics
+import 'autotrack';
+
+import rootReducer from './reducers';
+
+//  Import Components
+import App from './components/App';
+
+// Create store, apply middlewares etc
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(promise)),
+);
+
+// Create a function which will render a component to our DOM
+const render = Component => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Component />
+    </Provider>,
+    document.querySelector('#app'),
+  );
+};
+
+// Render the application
+render(App);
+
+// React HOT/HMR
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App);
+  });
+}
