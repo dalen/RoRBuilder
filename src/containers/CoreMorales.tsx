3486be9ef50cf5193f9cf1a01dc8b79480704a5a
Erik Dalén <erik.gustav.dalen@gmail.com>
2019-11-03 19:43:49 +0100

    Convert more files to TypeScript

---
+++ src/containers/CoreMorales.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import css from '../css/components/CoreMorales.module.css';
+
+import AbilityMorale from './AbilityMorale';
+
+import { State, Abilities } from '../reducers';
+
+const renderMorale = (abilities: Abilities, abilityId: number) => {
+  return (
+    <AbilityMorale
+      key={abilityId}
+      rank="4"
+      data={abilities.indexed[abilityId]}
+    />
+  );
+};
+
+const renderMorale4 = (abilities: Abilities, masteryMorales: number[]) => {
+  // Combine core and mastery tactics
+  let combinedMorales = [];
+  if (masteryMorales.length > 0) {
+    combinedMorales = [...abilities.structured.coreMorales4, ...masteryMorales];
+  } else {
+    combinedMorales = abilities.structured.coreMorales4;
+  }
+  return combinedMorales.map(morale => renderMorale(abilities, morale));
+};
+
+const renderMorales = (abilities: Abilities, masteryMorales: number[]) => {
+  return (
+    <div className={css.moraleContainer}>
+      <div className={css.moraleFirst}>
+        <h3 className={css.moraleHeading}>Rank 1</h3>
+        <div className="row">
+          {abilities.structured.coreMorales1.map(key => (
+            <AbilityMorale key={key} rank="1" data={abilities.indexed[key]} />
+          ))}
+        </div>
+      </div>
+      <div className={css.morale}>
+        <h3 className={css.moraleHeading}>Rank 2</h3>
+        <div className="row">
+          {abilities.structured.coreMorales2.map(key => (
+            <AbilityMorale key={key} rank="2" data={abilities.indexed[key]} />
+          ))}
+        </div>
+      </div>
+      <div className={css.morale}>
+        <h3 className={css.moraleHeading}>Rank 3</h3>
+        <div className="row">
+          {abilities.structured.coreMorales3.map(key => (
+            <AbilityMorale key={key} rank="3" data={abilities.indexed[key]} />
+          ))}
+        </div>
+      </div>
+      <div className={css.moraleLast}>
+        <h3 className={css.moraleHeading}>Rank 4</h3>
+        <div className="row">{renderMorale4(abilities, masteryMorales)}</div>
+      </div>
+    </div>
+  );
+};
+
+const CoreMorales = ({
+  abilities,
+  masteryMorales,
+}: ReturnType<typeof mapStateToProps>) => {
+  if (Array.isArray(abilities)) {
+    return null;
+  }
+  return (
+    <div className={css.container}>
+      <h2 className={css.heading}>Morales</h2>
+      {renderMorales(abilities, masteryMorales)}
+    </div>
+  );
+};
+
+function mapStateToProps({ abilities, masteryMorales }: State) {
+  return {
+    abilities,
+    masteryMorales,
+  };
+}
+
+export default connect(mapStateToProps)(CoreMorales);
---
+++ src/containers/CoreTactics.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import css from '../css/components/CoreAbilities.module.css';
+
+import AbilityTactic from './AbilityTactic';
+
+import { State, Abilities } from '../reducers';
+
+const renderTactic = (abilities: Abilities, abilityId: number) => {
+  return <AbilityTactic key={abilityId} data={abilities.indexed[abilityId]} />;
+};
+const renderTactics = (abilities: Abilities, masteryTactics: number[]) => {
+  // Combine core and mastery tactics
+  let combinedTactics = [];
+  if (masteryTactics.length > 0) {
+    combinedTactics = [...abilities.structured.coreTactics, ...masteryTactics];
+  } else {
+    combinedTactics = abilities.structured.coreTactics;
+  }
+  return combinedTactics.map(tactic => renderTactic(abilities, tactic));
+};
+
+const CoreTactics = ({
+  abilities,
+  masteryTactics,
+}: ReturnType<typeof mapStateToProps>) => {
+  if (Array.isArray(abilities)) {
+    return null;
+  }
+  return (
+    <div className={css.container}>
+      <h2 className={css.heading}>Tactics</h2>
+      <div className="row">{renderTactics(abilities, masteryTactics)}</div>
+    </div>
+  );
+};
+
+function mapStateToProps({ abilities, masteryTactics }: State) {
+  return {
+    abilities,
+    masteryTactics,
+  };
+}
+
+export default connect(mapStateToProps)(CoreTactics);
