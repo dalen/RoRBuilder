1b102d812464dbffc38a93b8dbfa6981574f099c
Erik Dalén <erik.gustav.dalen@gmail.com>
2019-11-03 20:53:07 +0100

    Convert App component to function

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Import components/containers
@@ -6,17 +6,15 @@ import Home from '../containers/Home';
 import Career from '../containers/Career';
 import NotFound from './NotFound';
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route path="/career/:slug/:careerSaved" component={Career} />
-          <Route path="/career/:slug" exact component={Career} />
-          <Route path="/" component={Home} />
-          <Route component={NotFound} />
-        </Switch>
-      </Router>
-    );
-  }
-}
+export default () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/career/:slug/:careerSaved" component={Career} />
+        <Route path="/career/:slug" exact component={Career} />
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
