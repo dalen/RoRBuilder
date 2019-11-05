84badf17ada0e4b9c2969b459c8bed4d99f9497a
Erik Dalén <erik.gustav.dalen@gmail.com>
2019-11-05 21:06:05 +0100

    Fix test

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -2,19 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger';
-import Home from './Home';
+import { createStore } from 'redux';
+import App from './App';
 import rootReducer from '../reducers';
 
 it('renders without crashing', () => {
   // Create store, apply middlewares etc
-  const loggerMiddleware = createLogger();
-  const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+  const store = createStore(rootReducer);
   const div = document.createElement('div');
   ReactDOM.render(
     <Provider store={store}>
-      <Home />
+      <App />
     </Provider>,
     div,
   );
