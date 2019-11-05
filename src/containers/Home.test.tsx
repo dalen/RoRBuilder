99750523794411df6e864164f74128fda8c1bcb5
Erik Dalén <erik.gustav.dalen@gmail.com>
2019-11-05 11:51:42 +0100

    Update middleware in test

--- src/containers/Home.test.tsx
+++ src/containers/Home.test.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import promise from 'redux-promise';
+import { createLogger } from 'redux-logger';
 import Home from './Home';
 import rootReducer from '../reducers';
 
 it('renders without crashing', () => {
   // Create store, apply middlewares etc
-  const store = createStore(rootReducer, applyMiddleware(promise));
+  const loggerMiddleware = createLogger();
+  const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
   const div = document.createElement('div');
   ReactDOM.render(
     <Provider store={store}>
