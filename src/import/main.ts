ebe814800f1f0afede4e410bb90563567f96cf45
Erik Dalén <erik.gustav.dalen@gmail.com>
2019-12-21 11:00:40 +0100

    Sync magus & SM

--- src/import/main.ts
+++ src/import/main.ts
@@ -430,6 +430,7 @@ const main = async () => {
   await validateCareer('marauder', CareerLine.MARAUDER, abilityData);
   await validateCareer('zealot', CareerLine.ZEALOT, abilityData);
   await validateCareer('magus', CareerLine.MAGUS, abilityData);
+  await validateCareer('sword-master', CareerLine.SWORD_MASTER, abilityData);
   await validateCareer(
     'disciple-of-khaine',
     CareerLine.DISCIPLE_OF_KHAINE,
